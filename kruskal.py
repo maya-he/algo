af6753c84306b5501739e25c8bda715dc01b3740
0xlol <72345439+futurevoid@users.noreply.github.com>
2024-05-19 03:25:50 +0300

    Changed Variable Names for clarity and added if statement to Avoid adding duplicate edges

--- kruskal.py
+++ kruskal.py
@@ -1,44 +1,43 @@
 graph = {
-    'A': {'B': 4, 'C': 4},
-    'B': {'A': 4, 'C': 2},
-    'C': {'A': 4, 'B': 2, 'D': 3, 'E': 2, 'F': 4},
-    'D': {'C': 3, 'F': 3},
-    'E': {'C': 2, 'F': 3},
-    'F': {'D': 3, 'C': 4, 'E': 3}
+    "A":{"B" : 3,"D" : 4},
+    "B":{"A" : 3,"C" : 1},
+    'C':{"D" : 2,"B" : 1},
+    "D":{"C" : 2,"A" : 4}
 }
 
-
 def kruskal(graph):
-
+    
     edges = []
-    for u in graph:
-        for v, weight in graph[u].items():
-            edges.append((weight, u, v))
-
+    for node in graph:
+        for adj_node, weight in graph[node].items():
+            if (weight, adj_node, node) not in edges:  # Avoid adding duplicate edges
+                edges.append((weight, node, adj_node))
+    
     edges.sort()
     mst = set()
-    parent = {vertex: vertex for vertex in graph}           #{A:A, B:A , ...}
+    parent = {vertex: vertex for vertex in graph}
     total_cost = 0
-
-    def find(x):
-        if parent[x] != x:
-            parent[x] = find(parent[x])
-        return parent[x]
-
-    def union(x, y):
-        root_x = find(x)
-        root_y = find(y)
-        if root_x != root_y:
-            parent[root_y] = root_x
-
-    for weight, u, v in edges:
-        if find(u) != find(v):
-            mst.add((u, v, weight))
-            union(u, v)
+    
+    def find_root(vertex):
+        if parent[vertex] != vertex:
+            parent[vertex] = find_root(parent[vertex])
+        return parent[vertex]
+    
+    def union(vertex1, vertex2):
+        root_v1 = find_root(vertex1)
+        root_v2 = find_root(vertex2)
+        if root_v1 != root_v2:
+            parent[root_v2] = root_v1
+        
+    for weight, node1, node2 in edges:
+        if find_root(node1) != find_root(node2):
+            mst.add((weight, node1, node2))
+            union(node1,node2)
             total_cost += weight
     return mst, total_cost
 
-
 minimum_spanning_tree, total_cost = kruskal(graph)
-print("Minimum Spanning Tree:", minimum_spanning_tree)
-print("Total Cost:", total_cost)
+
+print(f"Minimum Spanning Tree: {minimum_spanning_tree}")
+print(f"Total Cost: {total_cost}")
+ 
